da21fde9162c0a1863a223b624c1bd58dd28dec6
Khaled Emad <khaledemad656@gmail.com>
2022-05-10 22:54:31 +0200

    ...

--- script.js
+++ script.js
@@ -1,33 +1,124 @@
+var mode=localStorage.getItem("mode"); // game mode
+
 const guiBoard=document.getElementById("board");
-const playBtn=document.getElementById("playBtn");
+const afterGame=document.querySelector(".after-game");
 const label=document.getElementById("label");
 const coordinates={
     start:null,
     end:null,
 }
-
 let cells=guiBoard.children;
 let board=[
     [cells[0],cells[1],cells[2]],
     [cells[3],cells[4],cells[5]],
     [cells[6],cells[7],cells[8]],
 ];
-let player1={letter:"X",color:"red"};
-let player2={letter:"O",color:"blue"};
-let players=[player1,player2];
-let currentPlayer=players[Math.floor(Math.random()*players.length)];
+
+let scores={
+    X:-100,
+    O:100,
+    tie:0,
+};
+
+let turn=0;
+let isOver=false;
+let players=[{letter:"X",color:"red"},{letter:"O",color:"blue"}];
+let player1=players[0];
+let player2=players[1];
+let currentPlayer=players[0];
 
 function addEvent(cells){
     for (let cell of cells) {
         cell.addEventListener('click',()=>{
-            cell.innerText=currentPlayer.letter;
-            cell.style.color=currentPlayer.color;
-            checkForWin()
-            currentPlayer=(currentPlayer===player1) ? (player2) : (player1);
-            
-        }, { once: true })
+            makeMove(cell);
+        })
     }
-    
+}
+
+function makeMove(cell){
+    if(cell.dataset.clicked==="true") return;
+
+    cell.setAttribute("data-clicked","true");
+    cell.innerText=currentPlayer.letter;
+    cell.style.color=currentPlayer.color;
+    checkForWin();
+    turn+=1;
+    currentPlayer=(currentPlayer===player1) ? (player2) : (player1);
+
+    if(mode==="AI" && currentPlayer===player2 && isOver===false){
+        moveAI();
+    }
+}
+
+function moveAI(){
+    let bestScore=-Infinity;
+    let score=0;
+    let bestMove;
+    let bestPossibleMoves=[board[1][1],board[0][0]];
+
+    // make first move for the AI to boost speed
+    if(turn<3){
+        for(let cell of bestPossibleMoves){
+            if(cell.innerText===""){
+                makeMove(cell);
+                bestPossibleMoves.splice(bestPossibleMoves.indexOf(cell), 1);
+                return;
+            }
+        }
+    }
+
+    for(let row of board){
+        for(let cell of row){
+            if(cell.innerText===""){
+                cell.innerText='O'; //make a move
+                score=minimax(0,false); //evaluate the move 
+                cell.innerText=""; //undo the move
+                if(score > bestScore){ //chose the best move
+                    bestScore=score;
+                    bestMove=cell;
+                }
+            }
+        }
+    }
+    makeMove(bestMove);
+}
+
+function minimax(depth,isMax){
+
+    let result=checkForWin(true,isMax);
+    if(result !==null){
+        return scores[result];
+    }
+
+    if(isMax){
+        let bestScore=-Infinity;
+        let score=0;
+        for(let row of board){
+            for(let cell of row){
+                if(cell.innerText===""){
+                    cell.innerText='O'; //make a move
+                    score=minimax(depth+1,false); //evaluate ai move
+                    cell.innerText=""; //undo the move
+                    bestScore=Math.max(score, bestScore);
+                }
+            }
+        }
+        return bestScore;
+    }
+    else{
+        let minScore=Infinity;
+        for(let row of board){
+            for(let cell of row){
+                if(cell.innerText===""){
+                    cell.innerText='X'; //make a move
+                    let score=minimax(depth+1,true); //evaluate ai move
+                    cell.innerText=""; //undo the move
+                    minScore=Math.min(score, minScore);
+                }
+            }
+        }
+        return minScore;
+    }
 }
 
 function isEqual(a,b,c){
@@ -39,59 +130,88 @@ function isEqual(a,b,c){
     return equal;
 }
 
-function checkForWin(){
+function gameEnded(){
+    guiBoard.style.pointerEvents="none";
+    isOver=true;
+    afterGame.classList.remove('hidden');
+    label.style.color=currentPlayer.color;
+    label.innerText=`${currentPlayer.letter} wins!`;
+}
+
+function checkForWin(fromMinimax=false,isMax=false){
     //checkrows
     if(isEqual(board[0][0],board[0][1],board[0][2])||
     isEqual(board[1][0],board[1][1],board[1][2])||
     isEqual(board[2][0],board[2][1],board[2][2])){
-        drawLine("row",currentPlayer.color)
-        guiBoard.style.pointerEvents="none";
-        label.style.opacity=1;
-        label.style.color=currentPlayer.color;
-        label.innerText=`${currentPlayer.letter} wins!`;
-        playBtn.classList.remove('hidden');
+        if(fromMinimax){
+            if(isMax){ // IF isMax it is X turn
+                return 'X';
+            }
+            else{
+                return 'O';
+            }
+            
+        }
+        drawLine("row",currentPlayer.color);
+        gameEnded();
     }
     //check cols
     else if(isEqual(board[0][0],board[1][0],board[2][0])||
     isEqual(board[0][1],board[1][1],board[2][1])||
     isEqual(board[0][2],board[1][2],board[2][2])){
+        if(fromMinimax){
+            if(isMax){
+                return 'X';
+            }
+            else{
+                return 'O';
+            }
+            
+        }
         drawLine("col",currentPlayer.color)
-        guiBoard.style.pointerEvents="none";
-        label.style.opacity=1;
-        label.style.color=currentPlayer.color;
-        label.innerText=`${currentPlayer.letter} wins!`;
-        playBtn.classList.remove('hidden');
+        gameEnded();
     }
     //check diagnol 
     else if(isEqual(board[0][0],board[1][1],board[2][2])){
+        if(fromMinimax){
+            if(isMax){
+                return 'X';
+            }
+            else{
+                return 'O';
+            }
+            
+        }
         drawLine("diagnolleft",currentPlayer.color);
-        guiBoard.style.pointerEvents="none";
-        label.style.opacity=1;
-        label.style.color=currentPlayer.color;
-        label.innerText=`${currentPlayer.letter} wins!`;
-        playBtn.classList.remove('hidden');
+        gameEnded();
     }
     else if(isEqual(board[0][2],board[1][1],board[2][0])){
+        if(fromMinimax){
+            if(isMax){
+                return 'X';
+            }
+            else{
+                return 'O';
+            }
+            
+        }
         drawLine("diagnolright",currentPlayer.color);
-        guiBoard.style.pointerEvents="none";
-        label.style.opacity=1;
-        label.style.color=currentPlayer.color;
-        label.innerText=`${currentPlayer.letter} wins!`;
-        playBtn.classList.remove('hidden');
+        gameEnded();
     }
-
     else{
-        for(let i of board){
-            for(let j of i){
-                if(j.innerText===""){
-                    return
+        for(let row of board){
+            for(let cell of row){
+                if(cell.innerText===""){
+                    return null
                 }
             }
         }
-        label.style.opacity=1;
-        label.style.color="white";
+        if(fromMinimax)return 'tie';
+        guiBoard.style.pointerEvents="none";
+        isOver=true;
+        afterGame.classList.remove('hidden');
+        label.style.color="black";
         label.innerText="Draw!";
-        playBtn.classList.remove('hidden');
     }
 
 }
@@ -116,8 +236,6 @@ function drawLine(mode,color){
         xright:coordinates.end.getBoundingClientRect().right-parent.xleft,
         ybottom:coordinates.end.getBoundingClientRect().bottom-parent.ytop,
     };
-    console.log(start)
-    console.log(end)
 
     const svg=document.createElementNS("http://www.w3.org/2000/svg", 'svg');
     const line=document.createElementNS("http://www.w3.org/2000/svg", 'line');
@@ -157,6 +275,3 @@ function drawLine(mode,color){
 }
 
 addEvent(cells);
-
-
-

ef099623a785ca46555595036088a4ed3ace5d38
Khaled Emad <khaledemad656@gmail.com>
2022-05-26 22:46:12 +0200

    working on match making

--- src/js/multiplayer.js
+++ src/js/multiplayer.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
-import { getDatabase, ref, set, onDisconnect, } from "firebase/database";
+import { getDatabase, ref, set, onDisconnect, onValue, update } from "firebase/database";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBmE5Ccjk52L8ZcMLsJVpyFFmEKXFZHy2A",
@@ -11,27 +11,86 @@ const firebaseConfig = {
     messagingSenderId: "300642173930",
     appId: "1:300642173930:web:59f71e7c2e2b0cef5fe893"
 };
-
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
 
-let mode = localStorage.getItem("mode");
+function createRoom(playerID) {
+    let roomRef = ref(database, `rooms/${playerID}`);
+    set(roomRef, {
+        playerOne: {
+            id: playerID,
+            Letter: "X",
+            Color: "red",
+        },
+        playerTwo: {
+            id: "null",
+            Letter: "O",
+            Color: "blue",
+        }
+    });
+    return roomRef;
+}
+
+let hasRoom=false;
+function matchMaking(playerID) {
+    let allPlayersRef = ref(database, "players");
+    let allroomsRef = ref(database, "rooms");
+    let roomRef;
+    
+    onValue(allroomsRef, (snapshot) => {
+        let rooms = snapshot.val();
+        if(rooms){
+            if(!hasRoom){
+                for (let [key, value] of Object.entries(rooms)) {
+                    value=JSON.parse(JSON.stringify(value));
+                    let playertwo=value.playerTwo.id
+                    if(playertwo==="null"){
+                        // playertwo=playerID;
+                        roomRef=ref(database, `rooms/${value.playerOne.id}`);
+                        hasRoom=true;
+                        update(roomRef,{playerTwo:{id:playerID, Letter:'O', Color:"blue"}});
+                        break;
+                    }
+                }
+                if(!hasRoom){
+                    hasRoom=true;
+                    roomRef = createRoom(playerID);
+                }
+            }
+        }
+        else if (rooms === null) {
+            hasRoom=true;
+            roomRef = createRoom(playerID);
+        }
+    })
+    // onValue(allPlayersRef, (snapshot) => {
+    //     console.log(snapshot.val());
+    // })
+}
+
 (function () {
+    let mode = localStorage.getItem("mode");
     if (mode === "online") {
-        const auth = getAuth(app);
-        const database = getDatabase(app);
-        let playersRef=ref(database,"players");
-
         let playerID, playerRef;
 
         onAuthStateChanged(auth, (user) => {
             if (user) {
-                playerID=user.uid;
-                playerRef=ref(database,`players/${playerID}`);
-                set(playerRef,{
-                    id:playerID,
+                playerID = user.uid;
+                playerRef = ref(database, `players/${playerID}`);
+                set(playerRef, {
+                    id: playerID,
                     name: "Anonymous"
                 });
-                onDisconnect(playerRef).remove()
+                onDisconnect(playerRef).remove();
+                matchMaking(playerID);
+
+                //remove room if player disconnects
+                let roomRef = ref(database, `rooms/${playerID}`);
+                if (roomRef) {
+                    hasRoom=false;
+                    onDisconnect(roomRef).remove();
+                }
 
             } else {
                 console.log("user logged out!");
